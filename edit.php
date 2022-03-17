12359eb4794a447bdc23c7f1937c942004c95618
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-03-17 10:00:26 +0430

    set side bar to edit page

--- edit.php
+++ edit.php
@@ -21,31 +21,40 @@ if (isset ($_POST['name'])  && isset($_POST['email']) ) {
 
  ?>
 <?php require 'header.php'; ?>
-<div class="container">
-  <div class="card mt-5">
-    <div class="card-header">
-      <h2>Update person</h2>
+<div class="row p-0 m-0">
+    <div class="col-3  m-0 p-0">
+      <?php require('sidebar.php')?>
     </div>
-    <div class="card-body">
-      <?php if(!empty($message)): ?>
-        <div class="alert alert-success">
-          <?= $message; ?>
+    <div class="col-9">
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header">
+                    <h2>Update person</h2>
+                </div>
+                <div class="card-body">
+                    <?php if(!empty($message)): ?>
+                    <div class="alert alert-success">
+                        <?= $message; ?>
+                    </div>
+                    <?php endif; ?>
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input value="<?= $person->name; ?>" type="text" name="name" id="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" value="<?= $person->email; ?>" name="email" id="email"
+                                class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-info">Update person</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-      <?php endif; ?>
-      <form method="post">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input value="<?= $person->name; ?>" type="text" name="name" id="name" class="form-control">
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" value="<?= $person->email; ?>" name="email" id="email" class="form-control">
-        </div>
-        <div class="form-group">
-          <button type="submit" class="btn btn-info">Update person</button>
-        </div>
-      </form>
     </div>
-  </div>
 </div>
+
 <?php require 'footer.php'; ?>
