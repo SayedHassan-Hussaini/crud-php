28d1da252c81ab3e285b8ba8fae8653db3529083
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-07-29 21:02:49 +0430

    design loginpage

--- index.php
+++ index.php
@@ -1,46 +1,29 @@
-<?php
-require 'db.php';
-$sql = 'SELECT * FROM people';
-$statement = $connection->prepare($sql);
-$statement->execute();
-$people = $statement->fetchAll(PDO::FETCH_OBJ);
- ?>
-<?php require 'header.php'; ?>
+<!doctype html>
+<html lang="en">
 
-    <div class="row p-0 m-0">
-        <div class="col-3 m-0 p-0">
-            <?php require 'sidebar.php' ?>
-        </div>
-        <div class="col-9">
-            <div class="container">
-                <div class="card mt-5">
-                    <div class="card-header">
-                        <h2>All customer</h2>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-bordered">
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Action</th>
-                            </tr>
-                            <?php foreach($people as $person): ?>
-                            <tr>
-                                <td><?= $person->id; ?></td>
-                                <td><?= $person->name; ?></td>
-                                <td><?= $person->email; ?></td>
-                                <td>
-                                    <a href="edit.php?id=<?= $person->id ?>" class="btn btn-info">Edit</a>
-                                    <a onclick="return confirm('Are you sure you want to delete this entry?')"
-                                        href="delete.php?id=<?= $person->id ?>" class='btn btn-danger'>Delete</a>
-                                </td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php require 'footer.php'; ?>
+<head>
+    <title>Company</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="./style/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./style/index.css">
+        <script src="./style/js/poper.js"></script>
+        <script src="./style/js/bootstrap.min.js"></script>
+</head>
+
+<body class="bg-secondary">
+   <div class="minCard">
+   <form class="card loginCard" method="post" action="home.php">
+    <h1 class="headerLogin">Login page</h1>
+        <lable>User name:</lable>
+        <input name="username" type="text" />
+        
+        <lable>Password:</lable>
+        <input name="pass" type="password"/>
+        <button class="btn btn-info">Login</button>
+    </form>
+   </div>
+</body>
